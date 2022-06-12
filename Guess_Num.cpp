fe56ed5298d2a4fb0bd49c771e338344146ed195
Rayz_On <105559019+MiloPrysc@users.noreply.github.com>
2022-06-12 17:56:08 +0530

    Add files via upload

---
+++ Guess_Num.cpp
@@ -0,0 +1,58 @@
+/*
+***Welcome To Guess The Perfect Number***
+
+--> In this game there will be a random number generated which will be less than
+or equal to 25.
+
+  If user gives input of a number which is not equal to random number then the program will run/take
+  input until & unless it is not equal to the random number.
+
+  Author --> Rayzon/Milo_Prysco/PogZumi (Me only just 3 different names)
+  Rayzon/Milo_Prysco/PogZumi == RMP
+
+*/
+
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+
+  int rn;
+  srand(time(0));
+  rn = rand() % 25 + 1; // Generates random number in the range of 25
+
+  int a;
+
+  // cout << "Random number : " << rn << endl;
+  // Use this , if you can win lol 
+
+int tries = 0;
+
+  do{    
+    cout << "Enter the number : " << endl;
+    cin >> a;
+    if(a == rn){
+        cout << "Victory Royale!!!" << endl;
+        cout << "Tries : " << tries << endl;
+        cout << "Random_Num :  " << rn << endl;
+    }
+    else{
+        if(rn <= 10){
+           cout << "Hint : Number is less than or equal to 10" << endl;
+         }
+        else if(rn <= 20){
+           cout << "Hint : Number is less than or equal to 20" << endl;
+         }
+        else if(rn <= 25){
+           cout << "Hint : Number is less than or equal to 25" << endl;
+         }
+    }
+
+   tries++;
+  }while(a != rn);
+
+  return 0;
+}
