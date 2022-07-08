3c714822e3243c52944c7e7dffae9638b5760634
Rayz_On <105559019+MiloPrysc@users.noreply.github.com>
2022-07-08 21:27:44 +0530

    Create Binary_Convertor.cpp

---
+++ Binary_Convertor.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*  
+           
+           Generates a new binary number by transorming 0 to 1 && 1 to 0 ,
+           For - loop used to read the string
+
+*/
+
+class binary{ 
+  string bin; // bin = binary
+public:
+// Declaring Functions
+  void get_bin();
+  void chk_bin();
+  void convert_bin();
+  void out_bin();
+};
+
+// Gets the input from User
+void binary :: get_bin(){
+      cout << "Enter the binary number : " << endl;
+      getline(cin, bin);
+}
+
+// Checks the input if its in binary form(0 & 1 only)
+void binary :: chk_bin(){
+     for(int i = 0; i < bin.length(); i++){
+            if(bin.at(i) != '0' && bin.at(i) != '1'){
+                   cout << "Error_Code  01X013485X3T6" << endl;
+                   exit(0);
+            }
+            else{/* Blank Else statement */}
+     }
+}
+
+// Converts 0 to 1 && 1 to 0
+void binary :: convert_bin(){
+   chk_bin(); // Checking the input again()
+   for(int i = 0; i < bin.length(); i++){
+        if(bin.at(i) == '0'){
+            bin.at(i) = '1';
+        }
+        else if(bin.at(i) == '1'){
+            bin.at(i) = '0';
+        }
+   }
+}
+
+// Prints the generated output
+void binary :: out_bin(){
+     cout << "Binary number generated :  " << bin << endl;
+}
+
+// Driver Code 
+int main(){
+  
+  // Creating object && implementing the functions of our object's class
+  binary user1;
+  user1.get_bin();
+  user1.chk_bin();
+  user1.convert_bin();
+  user1.out_bin();
+
+  return 0;
+}
+
