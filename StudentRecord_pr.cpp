76779194daffae06f4c7da020c84ba4f62e668c2
Rayz_On <105559019+MiloPrysc@users.noreply.github.com>
2022-07-09 14:05:38 +0530

    Update StudentRecord_pr.cpp

--- StudentRecord_pr.cpp
+++ StudentRecord_pr.cpp
@@ -2,17 +2,7 @@
 #include<fstream>
 using namespace std;
 
-/* 
-
-Through this project, we can learn a lot about input/output streams and the file management system of C++.
- Our program collects student details like name, roll number, marks in each subject, and calculates their 
- grade. This is a simple console app. Note that we focus only on the correct inputs in this project, and 
- you can enhance it to handle wrong inputs. Here is the source code:
-
- Idea for the project by : hacker.io
- Written by : Me(Rayz_On)
-
-*/
+// Something in C++
 
 void create_student();
 void option();
